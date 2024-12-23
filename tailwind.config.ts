3ec9ad775b97197f9242eee0d33cc4a86bbde42c
Clint Brodar <clintbrodar@gmail.com>
2024-12-23 13:06:08 -0800

    create header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        tertiary: "var(--tertiary)",
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        text: "var(--text)",
       },
     },
   },
